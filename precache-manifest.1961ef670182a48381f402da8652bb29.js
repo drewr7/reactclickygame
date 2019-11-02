1c4e11ca336330f131f74012a8b1c0bf26c9cbc9
DrewReynolds <drew@sportscrm.com>
2019-11-01 23:41:00 -0500

    Updates

---
+++ precache-manifest.1961ef670182a48381f402da8652bb29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae76122c0f073c25f21f044baf7216d8",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "d0b3ba2dede40dd92c88",
+    "url": "/myapp/static/css/main.3cc85196.chunk.css"
+  },
+  {
+    "revision": "57d47a1d8b987d8b6e7a",
+    "url": "/myapp/static/js/2.1fc2cc99.chunk.js"
+  },
+  {
+    "revision": "d0b3ba2dede40dd92c88",
+    "url": "/myapp/static/js/main.9d59938f.chunk.js"
+  },
+  {
+    "revision": "1411f7bffa1336a0e0b7",
+    "url": "/myapp/static/js/runtime-main.e94485a6.js"
+  }
+]);
